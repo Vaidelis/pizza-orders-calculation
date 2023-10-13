702c8c048c85e16bb4fe9c412312188e0d915203
Vaidas <vaidasl07@gmail.com>
2023-10-13 14:28:26 +0300

    Test commit

--- PizzaOrdersCalculation/WeatherForecast.cs
+++ PizzaOrdersCalculation/WeatherForecast.cs
@@ -9,6 +9,5 @@ namespace PizzaOrdersCalculation
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
-        public int Temperature {  get; set; }
     }
 }
